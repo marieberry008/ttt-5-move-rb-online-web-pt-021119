ab1920b423a87a9801f710c328a45096c314165d
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-17 22:32:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(argument)
+argument = argument.to_i - 1
+end
+
+board = [” “, ” “, ” “]
+def update_array_at_with(array, index, value)
+ board[0]] = X
+end
